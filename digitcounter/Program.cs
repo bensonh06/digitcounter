9901ea71f5309cfd0a611a0f3d82e3883400f92a
jdjdhj uiuiui <g_inseng@outlook.com>
2024-02-23 14:36:57 +0000

    Add project files.

---
+++ digitcounter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitcounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] countArray = { 0,0,0,0,0,0,0,0,0,0 };
+
+            Console.Write("Input data: ");
+            string input = Console.ReadLine();
+
+            string[] inputArray = input.Split(',');
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                countArray[int.Parse(inputArray[i])]++;
+            }
+
+            for (int i = 0; i < countArray.Length; i++)
+            {
+                Console.Write("\n" + i + "\t" + countArray[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
